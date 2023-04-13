f352b0b051fa74515ef991df7104395594ec5792
Cassiano Peixoto <cassiano.peixoto@ardanis.com>
2023-04-13 18:35:49 +0100

    Creating tests for serialize and deserialize domain event

--- tests/CPX.Domain.Abstract.Test/Events/DomainEventTest.cs
+++ tests/CPX.Domain.Abstract.Test/Events/DomainEventTest.cs
@@ -1,4 +1,8 @@
+using System.Globalization;
 using CPX.Domain.Abstract.Events;
+using CPX.Domain.Abstract.Test.Mocks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace CPX.Domain.Abstract.Test.Events;
 
@@ -19,5 +23,51 @@ public class DomainEventTest
         Assert.Equal(id, domainEvent.AggregateId);
         Assert.Equal(version, domainEvent.Version);
         Assert.Equal(createdAt, domainEvent.CreatedAt);
+    }
+
+    [Fact]
+    public void Should_be_able_to_serialized_domain_event()
+    {
+        // Arrange
+        var aggregateId = Guid.Parse("65aee9c3-4d97-499a-943f-e8be93c31fd3");
+        var version = 1;
+        var createdAt = new DateTimeOffset(new DateTime(2013, 4, 13));
+        // Act
+        var @event = new FooDomainEvent(aggregateId, version, createdAt);
+        var serialized = SerializeObject(@event);
+        // Assert
+        Assert.Equal("{\"aggregateId\":\"65aee9c3-4d97-499a-943f-e8be93c31fd3\",\"version\":1,\"createdAt\":\"2013-04-13T00:00:00+01:00\"}", serialized);
+    }
+
+    [Fact]
+    public void Should_be_able_to_deserialize_domain_event()
+    {
+        // Arrange
+        var aggregateId = Guid.Parse("65aee9c3-4d97-499a-943f-e8be93c31fd3");
+        var version = 1;
+        var createdAt = new DateTimeOffset(new DateTime(2013, 4, 13));
+        // Act
+        var @event = new FooDomainEvent(aggregateId, version, createdAt);
+        var serialized = SerializeObject(@event);
+        var deserializedEvent = JsonConvert.DeserializeObject<FooDomainEvent>(serialized);
+        // Assert
+        Assert.NotNull(deserializedEvent);
+        if (deserializedEvent != null)
+        {
+            Assert.Equal(aggregateId.ToString(), deserializedEvent.AggregateId);
+            Assert.Equal(version, deserializedEvent.Version);
+            Assert.Equal(createdAt, deserializedEvent.CreatedAt);
+        }
+    }
+
+    private string SerializeObject(FooDomainEvent @event)
+    {
+        return JsonConvert.SerializeObject(@event, new JsonSerializerSettings()
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            DateFormatHandling = DateFormatHandling.IsoDateFormat,
+            DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+            Culture = CultureInfo.InvariantCulture
+        });
     }
 }
--- tests/CPX.Domain.Abstract.Test/Mocks/FooEvent.cs
+++ tests/CPX.Domain.Abstract.Test/Mocks/FooEvent.cs
@@ -4,7 +4,7 @@ namespace CPX.Domain.Abstract.Test.Mocks;
 
 public class FooDomainEvent : DomainEvent
 {
-    public FooDomainEvent(FooId aggregateId, int version, DateTimeOffset createdAt) : base(aggregateId, version, createdAt)
+    public FooDomainEvent(Guid aggregateId, int version, DateTimeOffset createdAt) : base(aggregateId, version, createdAt)
     {
     }
 }
